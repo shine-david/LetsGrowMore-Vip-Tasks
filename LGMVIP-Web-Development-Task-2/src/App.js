6fdf9c2d499353189ed515957be5ce074f808c67
shine-david <85192204+shine-david@users.noreply.github.com>
2021-08-24 00:27:52 +0530

    Add files via upload

---
+++ LGMVIP-Web-Development-Task-2/src/App.js
@@ -0,0 +1,49 @@
+import Users from "./components/cards";
+import './App.css';
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor(props){
+	super(props)
+		
+	// Set initial state
+	this.state = {users_data :([]),
+                loading : false                
+  }
+  
+
+	this.updateState = this.updateState.bind(this)
+  }
+    
+  updateState(){
+
+    this.setState({loading: true});
+
+      setTimeout(async function(){
+    
+      const response=await fetch("https://reqres.in/api/users?page=1");
+      const jsonresponse=await response.json();
+      console.log(jsonresponse)
+      this.setState({users_data:jsonresponse["data"],loading:false});
+      }.bind(this),2000);
+  }
+    
+  render(){
+  return (
+    <>
+    <nav className="navbar">
+    <div className="navitems">
+      <h2>LGM Task 2</h2>
+      <a name="" id="" class="btn btn-primary" onClick={this.updateState} role="button">Get Users</a>
+    </div>
+  </nav>
+  <div className="userdatacontainer">
+    <Users loading={this.state.loading} users={this.state.users_data}/>
+  </div>
+
+  </>
+  );
+  }
+}
+
+export default App;
